ada4fe4bb3ee14e2cf46f0910de73a0257987fe8
Shanmukh93 <112387984+Shanmukh93@users.noreply.github.com>
2026-04-12 17:52:29 +0530

    Update api.ts

--- services/api.ts
+++ services/api.ts
@@ -6,32 +6,50 @@ export const TMDB_CONFIG={
         Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
     }
 }
-
+ 
 export const fetchMovies = async ({ query }: { query: string }) => {
     const endpoint= query
     ?`${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
     :`${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
-
+ 
     const response = await fetch(endpoint, {
         method: 'GET',
         headers: TMDB_CONFIG.headers
     });
-    
+   
     if(!response.ok){
         throw new Error('Failed to fetch movies',response.statusText);
     }
   const data = await response.json();
   return data.results;
 }
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc%27;
+ 
+export const fetchMovieDetails = async (movieId: string): Promise<MovieDetails> => {
+    try{
+        const response = await fetch(`${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,{
+        method:'GET',
+        headers: TMDB_CONFIG.headers    
+    });
+    if(!response.ok){
+        throw new Error('Failed to fetch movie details',response.statusText);
+    }
+    const data = await response.json();
+    return data;
+}
+    catch(error){
+        throw new Error('Failed to fetch movie details');
+    }
+}
 // const options = {
+ 
 // method: 'GET',
 // headers: {
 // accept: 'application/json',
 // Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTcxOTkyYTk1MjlkZTE1OTFkNGM1MjFkNWVhYTBiYSIsIm5iZiI6MTc3NTM3NjkwMi4yMzYsInN1YiI6IjY5ZDIxYTA2MzdiZTU0YWMzNjA5MjMwMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YvA4dZdbHFhDVcGl-xZr8ATi4kUvrhu_xKx6IhM91zQ'
 // }
 // };
-
+ 
 // fetch(url, options)
 // .then(res => res.json())
 // .then(json => console.log(json))
