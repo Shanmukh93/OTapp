db5a35103b7665d6b946d3976eab68167f121977
Shanmukh93 <112387984+Shanmukh93@users.noreply.github.com>
2026-04-12 15:15:45 +0530

    Update SearchBar.tsx

--- Components/SearchBar.tsx
+++ Components/SearchBar.tsx
@@ -2,20 +2,21 @@ import { icons } from '@/constants/icons'
 import React from 'react'
 import { Image, TextInput, View } from 'react-native'
 
-const SearchBar = ({placeholder, onPress}: {placeholder: string, onPress: () => void}) => {
+ 
+const SearchBar = ({placeholder, onPress,value,onChangeText}: {placeholder: string, onPress?: () => void, value: string, onChangeText: (text: string) => void}) => {
   return (  
     <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-5'>
       <Image source={icons.search} className='w-5 h-5 mr-2' resizeMode='contain' tintColor='#ab8bff'/>
-      <TextInput 
+      <TextInput
         onPress={onPress}
         placeholder={placeholder}
-        value=''
-        onChangeText={()=>{}}
+        value={value}
+        onChangeText={onChangeText}
         placeholderTextColor="#a8b5db"
         className='flex-1 ml-2 text-white'
         />
     </View>
   )
 }
-
+ 
 export default SearchBar
